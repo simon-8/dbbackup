4a016a2e5305a692123d8c0b13fcc7cf8683aff6
simon <651138009@qq.com>
2018-05-13 21:04:14 +0800

    update readme.md

--- config.php
+++ config.php
@@ -1,18 +1,18 @@
 <?php
 //数据库名
-$database = 'zhuanqian';
+$database = '';
 //用户名
-$username = 'root';
+$username = '';
 //密码
-$password = '123456';
+$password = '';
 
 //给当前备份站点设置个名称，好在七牛云存储中分辨
 $webSite = 'test';
 
 
-//七牛相关 勿动
-$qiniuAK = 'GMPtaLAAOe8C7NCjUJz67tMvrSBlKxpouCGMx-A9';
+//七牛配置
+$qiniuAK = '';
 
-$qiniuSK = 'mdfM3Pmi0Ldi74giDpMaLDy1Z_gqPzWR0qSgSthH';
+$qiniuSK = '';
 
-$qiniuBucket = 'dbbackup';
+$qiniuBucket = '';
--- 添加环境变量.bat
+++ 添加环境变量.bat
@@ -1,5 +1,5 @@
 @echo off
-setx /M Path "%Path%;D:\UPUPW_AP5.3\PHP5;D:\UPUPW_AP5.3\MariaDB\bin"
+setx /M Path "%Path%;D:\Server\php7;D:\Server\MysqlServer\bin"
 chcp 936
 echo "添加成功，请检查。。。"
 pause
